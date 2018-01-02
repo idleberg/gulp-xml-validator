ffb41f584cdb962e2970ecc26aefc2bc9f5f3739
idleberg <jan@fragnebenan.com>
2018-01-02 10:21:34 +0100

    use Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,27 @@ var debug = require('gulp-debug');
 var jshint = require('gulp-jshint');
 var jsonlint = require('gulp-jsonlint');
 
-// Tasks
-gulp.task('lint', ['jshint', 'jsonlint']);
-
 // Exclude node_modules
 var self = '!node_modules/**/*';
 
 // Lint JavaScript files
-gulp.task('jshint', function() {
-    return gulp.src(['./**/*.js', self])
+gulp.task('jshint', (done) => {
+    gulp.src(['./**/*.js', self])
         .pipe(debug({title: 'jshint:'}))
         .pipe(jshint());
+    done()
 });
 
 // Lint JSON files
-gulp.task('jsonlint', function() {
+gulp.task('jsonlint', (done) => {
    gulp.src(['./**/*.json', self])
         .pipe(debug({title: 'jsonlint:'}))
         .pipe(jsonlint())
         .pipe(jsonlint.reporter());
+    done()
 });
+
+// Tasks
+gulp.task('lint', gulp.parallel('jshint', 'jsonlint', (done) => {
+  done();
+}));
