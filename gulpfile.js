8360bba9f38696375d53805706828efab39ebbad
idleberg <jan@fragnebenan.com>
2018-01-02 10:28:11 +0100

    fixes

--- gulpfile.js
+++ gulpfile.js
@@ -9,19 +9,21 @@ var self = '!node_modules/**/*';
 
 // Lint JavaScript files
 gulp.task('jshint', (done) => {
-    gulp.src(['./**/*.js', self])
-        .pipe(debug({title: 'jshint:'}))
-        .pipe(jshint());
-    done()
+  gulp.src(['./**/*.js', self])
+  .pipe(debug({title: 'jshint:'}))
+  .pipe(jshint());
+
+  done()
 });
 
 // Lint JSON files
 gulp.task('jsonlint', (done) => {
-   gulp.src(['./**/*.json', self])
-        .pipe(debug({title: 'jsonlint:'}))
-        .pipe(jsonlint())
-        .pipe(jsonlint.reporter());
-    done()
+  gulp.src(['./**/*.json', self])
+  .pipe(debug({title: 'jsonlint:'}))
+  .pipe(jsonlint())
+  .pipe(jsonlint.reporter());
+
+  done()
 });
 
 // Tasks
