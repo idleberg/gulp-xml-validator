cb93867fb51085427cde661a6c2141d8f4c0448a
Jan T. Sott <git@idleberg.com>
2023-07-03 00:23:42 +0200

    replace through2 package

--- index.mjs
+++ index.mjs
@@ -1,45 +1,48 @@
-import { DOMParser } from '@xmldom/xmldom'
+import { DOMParser } from '@xmldom/xmldom';
+import { Transform } from 'node:stream';
 import kleur from 'kleur';
 import PluginError from 'plugin-error';
-import through from 'through2';
 
 const packageName = 'gulp-xml-validator';
 
 export function xmlValidator() {
-  return through.obj(function (file, encoding, callback) {
-    if (file.isNull()) {
+  return new Transform({
+    objectMode: true,
+    transform(file, encoding, callback) {
+      if (file.isNull()) {
+        callback(null, file);
+        return;
+      }
+
+      if (file.isStream()) {
+        callback(new PluginError(packageName, 'Streaming not supported'));
+        return;
+      }
+
+      let errorList = [];
+
+      try {
+        new DOMParser({
+          locator: {},
+          errorHandler: function errorHandler(level, message) {
+            message = message.replace(/\[xmldom (warning|.*Error)\]\s+/g, '');
+            errorList.push(`${kleur.underline(file.relative)}: <${level}> ${message}`);
+          }
+        }).parseFromString(file.contents.toString(), 'text/xml');
+      } catch (err) {
+        this.emit('error', new PluginError(packageName, err, {
+          fileName: file.path
+        }));
+      }
+
+      if (errorList && errorList.length > 0) {
+        this.emit('error', new PluginError(packageName, `\n${errorList.join('\n')}`, {
+          fileName: file.path,
+          showStack: false
+        }));
+      }
+
       callback(null, file);
-      return;
     }
-
-    if (file.isStream()) {
-      callback(PluginError(packageName, 'Streaming not supported'));
-      return;
-    }
-
-    let errorList = [];
-
-    try {
-      new DOMParser({
-        locator: {},
-        errorHandler: function errorHandler(level, message) {
-          message = message.replace(/\[xmldom (warning|.*Error)\]\s+/g, '');
-          errorList.push(`${kleur.underline(file.relative)}: <${level}> ${message}`);
-        }
-      }).parseFromString(file.contents.toString(), 'text/xml');
-    } catch (err) {
-      this.emit('error', new PluginError(packageName, err, {
-				fileName: file.path
-			}));
-    }
-
-    if (errorList && errorList.length > 0) {
-      this.emit('error', new PluginError(packageName, `\n${errorList.join('\n')}`, {
-        fileName: file.path,
-        showStack: false
-      }));
-    }
-
-    callback(null, file);
   });
 }
